3bc042e974599bbaf97fafd4bb3a655bc669703f
Juan Pedraza <juan_pedraza@outlook.com>
2020-10-07 16:17:07 -0500

    Second commit

--- condicionales.py
+++ condicionales.py
@@ -1,7 +1,15 @@
-age = int((input("Escribe tu edad: ")))
+# age = int((input("Escribe tu edad: ")))
 
-if age > 18:
-    print("Eres mayor de edad")
-else:
-    print("No eres mayor de edad")
+# if age > 18:
+#     print("Eres mayor de edad")
+# else:
+#     print("No eres mayor de edad")
 
+number = int(input('Escribe un número: '))
+
+if number == 5:
+    print('Es igual a 5')
+elif number > 5:
+    print('Es mayor a 5')
+else: 
+    print('Es menor a 5')
