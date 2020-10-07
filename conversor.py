b154c596cdc214840bb0db2cdcb1e0861fd90ade
Juan Pedraza <juan_pedraza@outlook.com>
2020-10-07 18:43:44 -0500

    Functions

--- conversor.py
+++ conversor.py
@@ -1,9 +1,31 @@
-pesos = float(input("¿Cuántos pesos colombianos tienes?: "))
+menu = """
+Bienvenido al conversor de monedas
 
-dollar = 3800
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
 
-dollars = pesos / dollar
+Elige una opción: """
 
-dollars = round(dollars, 2)
+option = int(input(menu))
 
-print(f'Tienes {dollars} dólares al cambio')
+if option == 1:
+    pesos = float(input("¿Cuántos pesos colombianos tienes?: "))
+    dollar = 3800
+    dollars = pesos / dollar
+    dollars = round(dollars, 2)
+    print(f'Tienes {dollars} dólares al cambio')
+elif option == 2:
+    pesos = float(input("¿Cuántos pesos argenitos tienes?: "))
+    dollar = 65
+    dollars = pesos / dollar
+    dollars = round(dollars, 2)
+    print(f'Tienes {dollars} dólares al cambio')
+elif option == 3:
+    pesos = float(input("¿Cuántos pesos mexicanos tienes?: "))
+    dollar = 24
+    dollars = pesos / dollar
+    dollars = round(dollars, 2)
+    print(f'Tienes {dollars} dólares al cambio')
+else:
+    print('Ingrese una opción válida')
